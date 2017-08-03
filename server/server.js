7f083eef651227389b0f176e796bfb767e22962d
Panibrat <a.panibratenko@gmail.com>
2017-08-04 00:37:00 +0300

    2nd init

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on PORT =  ${port}`);
 });
